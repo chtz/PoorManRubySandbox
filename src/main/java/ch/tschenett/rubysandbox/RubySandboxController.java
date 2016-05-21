632148d4e5555988a6bf5d89f5562ed49beb18d9
Chris <chtz@me.com>
2016-05-21 22:11:58 +0200

    config introduced

--- src/main/java/ch/tschenett/rubysandbox/RubySandboxController.java
+++ src/main/java/ch/tschenett/rubysandbox/RubySandboxController.java
@@ -1,6 +1,7 @@
 package ch.tschenett.rubysandbox;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -9,6 +10,7 @@ import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,8 +19,8 @@ import org.springframework.web.bind.annotation.RestController;
  * sample: curl --header "Content-Type:application/octet-stream" --data-binary @../rubyRunContainer/sample.txt localhost:8080
  */
 @RestController
-public class IndexResource {
-	private static final Logger log = LoggerFactory.getLogger(IndexResource.class);
+public class RubySandboxController {
+	private static final Logger log = LoggerFactory.getLogger(RubySandboxController.class);
 	
 	@Value("${ruby.command}")
     private String rubyCommand;
@@ -29,10 +31,22 @@ public class IndexResource {
 	@Value("${ruby.workingDirectory}")
     private String rubyWorkingDirectory;
 	
-	@RequestMapping(value = "/", method = RequestMethod.POST)
-	public void addPet(InputStream req, OutputStream res) throws IOException {    
+	@Value("${ruby.scriptDirectory}")
+    private String rubyScriptDirectory;
+	
+	@RequestMapping(value = "/{scriptId}", method = RequestMethod.POST)
+	public void executeScript(InputStream req, OutputStream res, @PathVariable String scriptId) throws IOException {    
 		try {
-			ProcessBuilder pb = new ProcessBuilder(rubyCommand, "sample.rb", rubyTimeout);
+			File scriptDir = new File(rubyScriptDirectory);
+			File scriptFile = new File(scriptDir, scriptId + ".rb");
+			if (!scriptFile.getCanonicalPath().startsWith(scriptDir.getCanonicalPath())) {
+				throw new SecurityException();
+			}
+			if (!scriptFile.exists() || !scriptFile.canRead()) {
+				throw new FileNotFoundException();
+			}
+			
+			ProcessBuilder pb = new ProcessBuilder(rubyCommand, scriptFile.getCanonicalPath(), rubyTimeout);
 			pb.directory(new File(rubyWorkingDirectory));
 			
 			final Process p = pb.start();
@@ -70,7 +84,7 @@ public class IndexResource {
 			
 			p.waitFor();
 			
-			log.info("Request processed");
+			log.debug("Script processed: {}", scriptId);
 		} catch (Exception e) {
 			log.warn("Failed to process request", e);
 		}
