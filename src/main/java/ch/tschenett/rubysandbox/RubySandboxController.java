fa67ad575658575d5cc382887ce288f5a779a8c3
Chris <chtz@me.com>
2016-05-21 22:24:16 +0200

    upload feature added

--- src/main/java/ch/tschenett/rubysandbox/RubySandboxController.java
+++ src/main/java/ch/tschenett/rubysandbox/RubySandboxController.java
@@ -2,9 +2,13 @@ package ch.tschenett.rubysandbox;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.Reader;
+import java.io.Writer;
+import java.util.UUID;
 
 import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
@@ -16,7 +20,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * sample: curl --header "Content-Type:application/octet-stream" --data-binary @../rubyRunContainer/sample.txt localhost:8080
+ * samples:
+ * <pre>
+ * curl --header "Content-Type:application/octet-stream" --data-binary @../rubyRunContainer/sample.txt localhost:8080/74af008f-ea31-4896-a42f-7b82f581d3ce
+ * curl --header "Content-Type:application/octet-stream" --data-binary @testdata/scripts/sample.rb localhost:8080/
+ * </pre>
  */
 @RestController
 public class RubySandboxController {
@@ -34,19 +42,28 @@ public class RubySandboxController {
 	@Value("${ruby.scriptDirectory}")
     private String rubyScriptDirectory;
 	
+	@RequestMapping(value = "/", method = RequestMethod.POST)
+	public void createScript(Reader req, Writer res) throws IOException {
+		String scriptId = UUID.randomUUID().toString();
+		
+		File scriptDir = new File(rubyScriptDirectory);
+		File scriptFile = new File(scriptDir, scriptId + ".rb");
+		FileWriter out = new FileWriter(scriptFile);
+		try {
+			IOUtils.copy(req, out);
+		} finally {
+			out.close();
+		}
+		
+		res.write(scriptId);
+		
+		log.info("Script created: {}", scriptId);
+	}
+	
 	@RequestMapping(value = "/{scriptId}", method = RequestMethod.POST)
 	public void executeScript(InputStream req, OutputStream res, @PathVariable String scriptId) throws IOException {    
 		try {
-			File scriptDir = new File(rubyScriptDirectory);
-			File scriptFile = new File(scriptDir, scriptId + ".rb");
-			if (!scriptFile.getCanonicalPath().startsWith(scriptDir.getCanonicalPath())) {
-				throw new SecurityException();
-			}
-			if (!scriptFile.exists() || !scriptFile.canRead()) {
-				throw new FileNotFoundException();
-			}
-			
-			ProcessBuilder pb = new ProcessBuilder(rubyCommand, scriptFile.getCanonicalPath(), rubyTimeout);
+			ProcessBuilder pb = new ProcessBuilder(rubyCommand, scriptFile(scriptId).getCanonicalPath(), rubyTimeout);
 			pb.directory(new File(rubyWorkingDirectory));
 			
 			final Process p = pb.start();
@@ -84,9 +101,24 @@ public class RubySandboxController {
 			
 			p.waitFor();
 			
-			log.debug("Script processed: {}", scriptId);
+			log.info("Script processed: {}", scriptId);
 		} catch (Exception e) {
 			log.warn("Failed to process request", e);
 		}
+	}
+
+	private File scriptFile(String scriptId) throws IOException, FileNotFoundException {
+		File scriptDir = new File(rubyScriptDirectory);
+		File scriptFile = new File(scriptDir, scriptId + ".rb");
+		
+		if (!scriptFile.getCanonicalPath().startsWith(scriptDir.getCanonicalPath())) {
+			throw new SecurityException();
+		}
+		
+		if (!scriptFile.exists() || !scriptFile.canRead()) {
+			throw new FileNotFoundException();
+		}
+		
+		return scriptFile;
 	}
 }
