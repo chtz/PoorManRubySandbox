9c771d5992352eada3ae13d50ba99145a04def05
Chris <chtz@me.com>
2016-05-27 12:36:45 +0200

    minor tweaks

--- scripts/remote_ruby_get.sh
+++ scripts/remote_ruby_get.sh
@@ -1,5 +1,3 @@
 #/bin/bash
-echo "curl -s --header "Content-Type:text/plain" --data-binary @$1 $2/"
 SCRIPT_ID=`curl -s --header "Content-Type:text/plain" --data-binary @$1 $2/`
-echo "curl -s $2/$SCRIPT_ID$3"
 curl -s $2/$SCRIPT_ID$3 
---
+++ scripts/test_get_cloud.sh
@@ -0,0 +1,2 @@
+#/bin/bash
+./scripts/remote_ruby_get.sh testdata/scripts/sample.rb http://www.sandbox.p.iraten.ch "?foo=bar"
