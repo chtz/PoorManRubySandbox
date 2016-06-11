5006e28e76b700d22aafd4a84aa5304b086d6202
Christian Tschenett <chtz@me.com>
2016-06-11 12:41:23 +0200

    sample extended

--- scripts/test_workflow_cloud.sh
+++ scripts/test_workflow_cloud.sh
@@ -6,3 +6,7 @@ IN_ID=`curl -s --header "Content-Type:text/plain" --data-binary '{"result":900}'
 TOK_ID=`curl -s http://www.sandbox.p.iraten.ch/wf/$WF_ID/$IN_ID | jq -r .token.childs[0].uuid`
 IN_ID=`curl -s --header "Content-Type:text/plain" --data-binary '{"result":200}' http://www.sandbox.p.iraten.ch/wf/$WF_ID/$IN_ID/$TOK_ID`
 curl -s http://www.sandbox.p.iraten.ch/wf/$WF_ID/$IN_ID | json_pp
+sleep 60
+curl -s http://www.sandbox.p.iraten.ch/wf/$WF_ID/$IN_ID | json_pp
+sleep 60
+curl -s http://www.sandbox.p.iraten.ch/wf/$WF_ID/$IN_ID | json_pp
