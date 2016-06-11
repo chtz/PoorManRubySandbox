b2b61c01449e29534c62dddf2382dcec6ba72f9f
Christian Tschenett <chtz@me.com>
2016-06-11 14:06:44 +0200

    debug out

--- scripts/test_workflow_cloud.sh
+++ scripts/test_workflow_cloud.sh
@@ -1,7 +1,10 @@
 #/bin/bash
 WF_ID=`curl -s --header "Content-Type:text/plain" --data-binary @testdata/scripts/test3_wf.rb http://www.sandbox.p.iraten.ch/wf`
+echo $WF_ID=curl -s --header "Content-Type:text/plain" --data-binary @testdata/scripts/test3_wf.rb http://www.sandbox.p.iraten.ch/wf
 IN_ID=`curl -s --header "Content-Type:text/plain" --data-binary '{"sum":123}' http://www.sandbox.p.iraten.ch/wf/$WF_ID`
+echo $IN_ID=curl -s --header "Content-Type:text/plain" --data-binary '{"sum":123}' http://www.sandbox.p.iraten.ch/wf/$WF_ID
 TOK_ID=`curl -s http://www.sandbox.p.iraten.ch/wf/$WF_ID/$IN_ID | jq -r .token.childs[0].uuid`
+echo $TOK_ID=curl -s http://www.sandbox.p.iraten.ch/wf/$WF_ID/$IN_ID | jq -r .token.childs[0].uuid
 IN_ID=`curl -s --header "Content-Type:text/plain" --data-binary '{"result":900}' http://www.sandbox.p.iraten.ch/wf/$WF_ID/$IN_ID/$TOK_ID`
 TOK_ID=`curl -s http://www.sandbox.p.iraten.ch/wf/$WF_ID/$IN_ID | jq -r .token.childs[0].uuid`
 IN_ID=`curl -s --header "Content-Type:text/plain" --data-binary '{"result":200}' http://www.sandbox.p.iraten.ch/wf/$WF_ID/$IN_ID/$TOK_ID`
