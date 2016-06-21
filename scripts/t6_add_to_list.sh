00d74e52c1c4d01297c83e36943269c28ceb3d9e
Christian Tschenett <chtz@me.com>
2016-06-21 23:56:24 +0200

    adjusted sample

--- scripts/t6_add_to_list.sh
+++ scripts/t6_add_to_list.sh
@@ -2,4 +2,5 @@
 WF_ID=$(head -n1 testdata/t6_wf.id)
 IN_ID=$(head -n1 testdata/t6_in.id)
 
-IN_ID=`curl -s --header "Content-Type:text/plain" --data-binary "{\"more\":\"$1\"}" http://www.sandbox.p.iraten.ch/wf/$WF_ID/$IN_ID`
+echo IN_ID=curl -s --header "Content-Type:text/plain" --data-binary "{\"more\":$(jshon -s "$@")}" http://www.sandbox.p.iraten.ch/wf/$WF_ID/$IN_ID
+IN_ID=`curl -s --header "Content-Type:text/plain" --data-binary "{\"more\":$(jshon -s "$@")}" http://www.sandbox.p.iraten.ch/wf/$WF_ID/$IN_ID`
