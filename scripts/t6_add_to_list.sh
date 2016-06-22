4148af109a86392b3acc751f4d618dbf25637620
Christian Tschenett <chtz@me.com>
2016-06-22 20:40:15 +0200

    minor tweaks

--- scripts/t6_add_to_list.sh
+++ scripts/t6_add_to_list.sh
@@ -3,4 +3,5 @@ WF_ID=$(head -n1 testdata/t6_wf.id)
 IN_ID=$(head -n1 testdata/t6_in.id)
 
 echo IN_ID=curl -s --header "Content-Type:text/plain" --data-binary "{\"more\":$(jshon -s "$@")}" http://www.sandbox.p.iraten.ch/wf/$WF_ID/$IN_ID
+
 IN_ID=`curl -s --header "Content-Type:text/plain" --data-binary "{\"more\":$(jshon -s "$@")}" http://www.sandbox.p.iraten.ch/wf/$WF_ID/$IN_ID`
