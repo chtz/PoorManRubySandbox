0bd7fedaccca23a881586adc1d99a62fa3883e9c
Christian Tschenett <chtz@me.com>
2016-12-16 00:45:43 +0100

    https support

--- scripts/t7_get.sh
+++ scripts/t7_get.sh
@@ -2,4 +2,4 @@
 WF_ID=$(head -n1 testdata/t7_wf.id)
 IN_ID=$(head -n1 testdata/t7_in.id)
 
-curl -s http://www.sandbox.p.iraten.ch/wf/$WF_ID/$IN_ID| jq -r .token.variables.more2
+curl -s https://www.sandbox.p.iraten.ch/wf/$WF_ID/$IN_ID| jq -r .token.variables.more2
--- scripts/t7_new.sh
+++ scripts/t7_new.sh
@@ -1,7 +1,7 @@
 #/bin/bash
-WF_ID=`curl -s --header "Content-Type:text/plain" --data-binary @testdata/scripts/test7_wf.rb http://www.sandbox.p.iraten.ch/wf`
+WF_ID=`curl -s --header "Content-Type:text/plain" --data-binary @testdata/scripts/test7_wf.rb https://www.sandbox.p.iraten.ch/wf`
 
-IN_ID=`curl -s --header "Content-Type:text/plain" --data-binary '{}' http://www.sandbox.p.iraten.ch/wf/$WF_ID`
+IN_ID=`curl -s --header "Content-Type:text/plain" --data-binary '{}' https://www.sandbox.p.iraten.ch/wf/$WF_ID`
 
 echo $WF_ID > testdata/t7_wf.id
 echo $IN_ID > testdata/t7_in.id
